501dda0dc6c2020e889c46a242435bed41af3b2a
Lau <laurent.henriques@gmail.com>
2019-03-06 22:51:22 +0100

    add button and more responsive

--- menu.js
+++ menu.js
@@ -1,11 +1,14 @@
 $(document).ready(function(){
-  $(".play").click(function(){
+  $("#show1").click(function(){
     $(".play").hide(1000);
   });
   $("#show").hover(function(){
     $(".border2").show(500).css('display', 'flex');
   });
-   $("#show").click(function(){
+   $("#show1").click(function(){
     $(".nes-container").show(1000).css('display', 'flex');
+  });
+  $("#show1").click(function(){
+    $(".border2").show(1000).css('display', 'flex');
   });
 });
