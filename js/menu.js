0b36bd27bb37d8e3f48197fd3f6bf5f39126ac34
LaurentHenriques <46579193+LaurentHenriques@users.noreply.github.com>
2019-03-14 17:21:28 +0100

    Update menu.js

--- js/menu.js
+++ js/menu.js
@@ -17,15 +17,20 @@ $(document).ready(function(){
   
 });
 
-
-  function imgchange(element) {
-  var x = document.getElementById('imgswitch')
-
-  x.setAttribute("src","https://ubistatic19-a.akamaihd.net/resource/fr-ca/game/rabbids/portal/yoshi%20cover_301841.jpg")
-  }
-
-  function imgchange2(element) {
+ function imgchange2(element) {
     var x = document.getElementById('imgswitch')
+    var v = x.getAttribute("src");
+
+    if (v == 'https://i.ytimg.com/vi/srXw1ayn5Fs/maxresdefault.jpg') {
+    
   
     x.setAttribute("src","https://www.actugaming.net/wp-content/uploads/2018/09/Yoshi-Switch.jpg")
+  }
+
+  if (v == 'https://www.actugaming.net/wp-content/uploads/2018/09/Yoshi-Switch.jpg') {
+    x.setAttribute("src","https://ubistatic19-a.akamaihd.net/resource/fr-ca/game/rabbids/portal/yoshi%20cover_301841.jpg")
+  }
+  if (v == 'https://ubistatic19-a.akamaihd.net/resource/fr-ca/game/rabbids/portal/yoshi%20cover_301841.jpg') {
+    x.setAttribute("src","https://i.ytimg.com/vi/srXw1ayn5Fs/maxresdefault.jpg")
+  }
     }
