5652fc52f9f48c2cbc972154601f1f1c5bd14e7a
David <belmontecast@gmail.com>
2019-03-15 11:45:13 +0100

    formulaire

---
+++ js/footer.js
@@ -0,0 +1,13 @@
+
+
+function openWin(url){
+    newwin=window.open(url,'','width=400,height=400,top=200,left=200');
+    if(newwin){
+    window.onfocus=function(){newwin.window.close()}
+    }
+    }
+
+function merci(){
+    var elem = document.getElementById('merci')
+    return window.alert('Yoshi vous remercie et vous enverra vos oeufs au plus vite !!!')
+}
