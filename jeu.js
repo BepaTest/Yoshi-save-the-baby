39685de40c215a739e6171e7e7eb0bb146d3c574
Wozzz82 <45991372+Wozzz82@users.noreply.github.com>
2019-03-15 13:35:49 +0100

    Update jeu.js

--- jeu.js
+++ jeu.js
@@ -1,2 +1,15 @@
 const backJeu = document.getElementById("jacket")
 backJeu.style.background = "rgb(253, 203, 109)";
+
+
+function openWin(url){
+    newwin=window.open(url,'','width=400,height=400,top=200,left=200');
+    if(newwin){
+    window.onfocus=function(){newwin.window.close()}
+    }
+    }
+
+function merci(){
+    var elem = document.getElementById('merci')
+    return window.alert('Yoshi vous remercie et vous enverra vos oeufs au plus vite !!!')
+}
