55a450a17157958a913c2426f0b4ec6101a3e77d
Vera Testard <vera.testard@gmail.com>
2019-03-15 11:35:52 +0100

    changement 3 images

--- js/histoireYoshi.js
+++ js/histoireYoshi.js
@@ -1,9 +1,14 @@
 
 
 const oeuf = document.getElementById("image0");
-oeuf.addEventListener('click', changeImage)
 
-function changeImage()
-{
- oeuf.setAttribute("src", "images/babyyoshi2.png")
+function changeImage() {
+    console.log(oeuf.src.indexOf("images/oeufRose.png")>=0)
+    if (oeuf.src.indexOf("images/oeufRose.png")>=0)
+        oeuf.src = "./images/babyyoshi2.png";
+    else if (oeuf.src.indexOf("images/babyyoshi2.png")>=0)
+        oeuf.src = "./images/vide.png";
+    else 
+        oeuf.src = "./images/oeufRose.png";
 }
+
