a70b10e98ec0b0fcdad38ba4ab2775e004dbc5ab
Justine Vareille <vareille.justine@gmail.com>
2019-03-22 13:10:12 +0100

    modif page

--- jeu.js
+++ jeu.js
@@ -1,2 +1,12 @@
 const backJeu = document.getElementById("jacket")
 backJeu.style.background = "rgb(253, 203, 109)";
+
+/* bouton achat*/
+
+$( document ).ready(function() {
+    $('.trigger').click(function() {
+       $('.modal-wrapper').toggleClass('open');
+      $('.page-wrapper').toggleClass('blur');
+       return false;
+    });
+  });
